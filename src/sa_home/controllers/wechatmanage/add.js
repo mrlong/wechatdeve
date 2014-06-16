62d98adaa71f91af9ad56d36429e4976f335a128
龙仕云 <mrlong.com@gmail.com>
2014-06-16 18:55:38 +0800

    show

--- src/sa_home/controllers/wechatmanage/add.js
+++ src/sa_home/controllers/wechatmanage/add.js
@@ -10,11 +10,48 @@
 
 var express = require('express');
 var router = express.Router();
+var Obj = require('../../../lib/obj');
+var Db = require('../../../lib/db');
+var Util = require('../../../lib/util');
 
 
 router.get('/add',function(req,res,next){
 	res.render('wechatmanage/add',{req_path:'/home/wechatmanage/'});
 });
 
+//增加
+router.post('/add/post',function(req,res,next){
+  //1.取出参数
+   var data = new Obj({
+    wech_name: req.body.wech_name,
+    wech_account: req.body.wech_account,
+    wech_access_token: req.body.wech_access_token,
+    wech_signature:req.body.wech_signature     
+  });
+  
+  data.trim().xss();
+  
+  data.wech_guid = Util.newGuid();
+  data.wech_type = 1;
+  data.memb_guid = req.session.curruser.memb_guid;
+  data.megr_guid = req.session.curruser.megr_guid;
+  data.wech_token = '';
+  data.wech_original='';
+  Db.query('INSERT INTO ims_wechats SET ?',data,function(err,results){
+    //console.log(results);
+    if(!err && results.affectedRows>0){
+      res.render('wechatmanage/add_success',{req_path:'/home/wechatmanage/',success:true});
+    }
+    else {
+      res.render('wechatmanage/add_success',{req_path:'/home/wechatmanage/',success:false});
+    }
+  });
+  
+});
+
+//
 
 module.exports = router;
+
+
+
--- src/sa_home/controllers/wechatmanage/index.js
+++ src/sa_home/controllers/wechatmanage/index.js
@@ -10,10 +10,25 @@
 
 var express = require('express');
 var router = express.Router();
-
+var Db = require('../../../lib/db');
 
 router.get('/',function(req,res,next){
-	res.render('wechatmanage/index',{});
+    var data={};
+    data.memb_guid = req.session.curruser.memb_guid;
+    data.megr_guid = req.session.curruser.megr_guid;
+    
+    Db.query('SELECT * FROM ims_wechats where megr_guid=?',[data.megr_guid],function(err,results){
+      console.log(results);
+      
+      if(!err){
+           res.render('wechatmanage/index',{datas:results});
+      }
+      else{
+        res.render('wechatmanage/index',{datas:null});
+      }
+    });
+  
+	
 });
 
 
